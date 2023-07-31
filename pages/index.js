20499df0f5cbed5e93cfa5e78ad2ceb40aa28ef0
Galammar <80973810+Galammar@users.noreply.github.com>
2023-07-31 17:58:54 +0300

    added viewer (incomplete)

--- pages/index.js
+++ pages/index.js
@@ -4,13 +4,15 @@ import { useState } from 'react';
 import React from 'react';
 
 export default function Home() {
+  let [currentPage, setCurrentPage] = useState('articles');
+  let [viewingArticleID, setViewingArticleID] = useState(1);
   let [library, setLibrary] = useState([
     {
-      title: 'Greeting',
+      title: 'Greetings',
       preface: 'Hello World',
       content: 'Hello World!',
       date: '13/7/23',
-      ID: 1,
+      id: 1,
     },
     {
       title: 'Lorem Ipsum',
@@ -18,11 +20,11 @@ export default function Home() {
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam eu dui sed lectus lobortis placerat sed ac diam. Donec turpis velit, scelerisque a tempor vitae, faucibus et dui. Etiam tincidunt velit id magna sagittis, finibus accumsan ante hendrerit. Suspendisse potenti. Vivamus eget ante sodales, placerat leo eget, bibendum metus. Sed id erat id sem vulputate dignissim vel eu ipsum. Etiam ac purus purus. In quis quam ut leo porta porta sed vitae velit. Sed dapibus bibendum lorem eu tristique. Maecenas magna ligula, gravida a tellus id, gravida suscipit dolor. Donec tincidunt nisl eget leo tempor commodo.',
       content: '',
       date: '13/7/23',
-      ID: 2,
+      id: 2,
     },
   ]);
 
-  function Article({ title, preface }) {
+  function Article({ title, preface, id }) {
     return (
       <div className={styles.article}>
         <div>
@@ -30,7 +32,15 @@ export default function Home() {
           <div
             dangerouslySetInnerHTML={{ __html: truncateString(preface, 615) }}
           />
-          <button className={styles.articleButton}>{'Read More >'}</button>
+          <button
+            className={styles.articleButton}
+            onClick={() => {
+              setViewingArticleID(id);
+              setCurrentPage('viewer');
+            }}
+          >
+            {'Read More >'}
+          </button>
         </div>
       </div>
     );
@@ -44,6 +54,7 @@ export default function Home() {
             key={index}
             title={article.title}
             preface={article.preface}
+            id={article.id}
           />
         ))}
       </div>
@@ -54,14 +65,48 @@ export default function Home() {
     return (
       <div className={styles.navBar}>
         <span className={styles.navBarElements}>
-          <h2>Home</h2>
+          <h2 onClick={() => setCurrentPage('home')}>Home</h2>
         </span>
         <span className={styles.navBarElements}>
-          <h2>Articles</h2>
+          <h2 onClick={() => setCurrentPage('articles')}>Articles</h2>
         </span>
         <span className={styles.navBarElements}>
-          <h2>New +</h2>
+          <h2 onClick={() => setCurrentPage('new')}>New +</h2>
         </span>
+      </div>
+    );
+  }
+
+  function Home() {
+    return (
+      <div>
+        <h1>Home</h1>
+      </div>
+    );
+  }
+
+  function Articles() {
+    return (
+      <div>
+        <Diary />
+      </div>
+    );
+  }
+
+  function New() {
+    return (
+      <div>
+        <h1>New</h1>
+      </div>
+    );
+  }
+
+  function Viewer() {
+    let thisArticle = library.find((item) => item.id === viewingArticleID);
+    return (
+      <div>
+        <h1>{thisArticle.title}</h1>
+        {thisArticle.content}
       </div>
     );
   }
@@ -81,7 +126,15 @@ export default function Home() {
       </Head>
       <main>
         <NavBar />
-        <Diary />
+        {currentPage === 'new' ? (
+          <New />
+        ) : currentPage === 'articles' ? (
+          <Articles />
+        ) : currentPage === 'viewer' ? (
+          <Viewer />
+        ) : (
+          <Home />
+        )}
       </main>
     </div>
   );
